be66487c7f5a3417984c5f7688aeafbf408c53a7
xiehuasheng <xiehuasheng@appublisher.com>
2019-01-31 16:02:39 +0800

    ie浏览器测试

---
+++ chapter15/brief03/js/2.js
@@ -0,0 +1,11 @@
+alert("search", window.location.search);
+alert("search", window.location.search);
+// function getQueryString(name) {
+//   let search = window.location.search.substr(1);
+//   let reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+//   let r = search.match(reg);
+//   if (r !== null) {
+//     return unescape(r[2]);
+//   }
+//   return "";
+// }
