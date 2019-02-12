efee0319e359d38809405ac69512ebb8edd23909
yangjingjing <yangjingjing@appublisher.com>
2019-02-12 17:22:44 +0800

    Iterator(遍历器)的概念代码练习

---
+++ chapter16/brief01/js/2.js
@@ -0,0 +1,12 @@
+var it = idMaker();
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
+function idMaker() {
+  var index = 0;
+  return {
+    next: function () {
+      return { value: index++, done: false };
+    }
+  };
+}
---
+++ chapter16/brief01/js/3.js
@@ -0,0 +1,13 @@
+// 遍历器接口Iterable
+// interface Iterable{
+//   [Symbol.iterator]():Iterator,
+// }
+// 指针对象Iterator
+// interface Iterator{
+//   next(value?:any):IterationResult,
+// }
+// next方法
+// interface IterationResult{
+//   value:any,
+//   done:boolean
+// }
