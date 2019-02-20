35decae9d9a0ada5d9dae4183ab4346cd696c76e
yangjingjing <yangjingjing@appublisher.com>
2019-02-20 18:40:18 +0800

    class代码练习

--- chapter20/brief01/js/8.js
+++ chapter20/brief01/js/8.js
@@ -14,3 +14,11 @@ console.log(point.hasOwnProperty('x'));
 console.log(point.hasOwnProperty('y'));
 console.log(point.hasOwnProperty('toString'));
 console.log(point.__proto__.hasOwnProperty('toString'));
+/**
+ * 上面代码中,x和y都是实例对象point自身的属性(因为定义在this变量上),所以hasOwnProperty方法返回true
+ * 而toString是原型对象的属性(因为定义在Point类上),所以hasOwnProperty方法返回false.
+ */
+// 与es5一样,类的所有实例共享一个原型对象
+var p1 = new Point(2, 3);
+var p2 = new Point(3, 2);
+console.log(p1.__proto__ === p2.__proto__);
